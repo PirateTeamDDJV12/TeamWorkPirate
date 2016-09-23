e0139f19aadcfa6a46c0360b5724cc44f2b5fceb
Sun-Lay Gagneux <gags1920@usherbrooke.ca>
2016-09-23 17:22:32 -0400

    Triangle class squeletton
    Squeletton. Nothing declared or implemented.
    
    Just added an header and a cpp file for a triangle class. Nothing done
    or written here. Just a squeletton.

---
+++ Projets/FieldConverter/FieldConverter/Triangle.cpp
@@ -0,0 +1,7 @@
+/*
+Created by Sun-lay Gagneux
+*/
+#include "Triangle.h"
+
+
+using namespace FieldConverter;
---
+++ Projets/FieldConverter/FieldConverter/Triangle.h
@@ -0,0 +1,17 @@
+/*
+Created by Sun-lay Gagneux
+*/
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+
+namespace FieldConverter
+{
+    class Triangle
+    {
+
+    };
+}
+
+
+#endif //!TRIANGLE_H
