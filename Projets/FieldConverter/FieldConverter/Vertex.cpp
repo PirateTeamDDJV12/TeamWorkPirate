63a1088bf93f5cceba0471d35286a354261cd32c
Constant Brunel <constant.95@gmail.com>
2016-09-25 22:36:07 -0400

    Fix bad implementation toString Vertex

--- Projets/FieldConverter/FieldConverter/Vertex.cpp
+++ Projets/FieldConverter/FieldConverter/Vertex.cpp
@@ -9,9 +9,9 @@ using namespace FieldConverter;
 
 void Vertex::writeVectIntoStdString(std::stringstream& container, const Vect3f& valueToWrite) const noexcept
 {
-    container << this->position().x() << SPACE_SEPARATOR;
-    container << this->position().y() << SPACE_SEPARATOR;
-    container << this->position().z();
+    container << valueToWrite.x() << SPACE_SEPARATOR;
+    container << valueToWrite.y() << SPACE_SEPARATOR;
+    container << valueToWrite.z();
 }
 
 std::string Vertex::toString() const noexcept
