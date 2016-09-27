9e5904ea1ceda60d95ca0c061d324875e0386444
Alexandre Sendra <sendra.alexandre@gmail.com>
2016-09-27 15:50:25 -0400

    remove an include from main

--- Projets/FieldConverter/FieldConverter/main.cpp
+++ Projets/FieldConverter/FieldConverter/main.cpp
@@ -12,7 +12,6 @@ Created by Sun-lay gagneux
 #include "UnitaryTestTriangleArray.h"
 #include "Config.hpp"
 #include <iostream>
-#include <stdexcept>
 #include <algorithm>
 #include <map>
 
@@ -25,145 +24,127 @@ using namespace std;
 
 void run(const Vect3f& vect1, const Vect3f& vect2)
 {
-    cout << "vect1(" << vect1.x() << " " << vect1.y() << " " << vect1.z() << ")" << endl;
-    cout << "vect2(" << vect2.x() << " " << vect2.y() << " " << vect2.z() << ")" << endl;
+	cout << "vect1(" << vect1.x() << " " << vect1.y() << " " << vect1.z() << ")" << endl;
+	cout << "vect2(" << vect2.x() << " " << vect2.y() << " " << vect2.z() << ")" << endl;
 
-    float result1 = vect1.scalarProduct(vect2);
-    float result2 = vect2.scalarProduct(vect1);
+	float result1 = vect1.scalarProduct(vect2);
+	float result2 = vect2.scalarProduct(vect1);
 
-    if(result1 != result2)
-    {
-        throw 1;
-    }
+	if (result1 != result2)
+	{
+		throw 1;
+	}
 
-    cout << "scalar product : " << result1 << endl;
-    cout << "scalar product : " << result2 << endl;
+	cout << "scalar product : " << result1 << endl;
+	cout << "scalar product : " << result2 << endl;
 
-    Vect3f vect3 = Vect3f::crossProduct(vect1, vect2);
+	Vect3f vect3 = Vect3f::crossProduct(vect1, vect2);
 
-    if((Vect3f::scalarProduct(vect3, vect1) != 0.f) || (Vect3f::scalarProduct(vect3, vect2) != 0.f))
-    {
-        throw 2;
-    }
+	if ((Vect3f::scalarProduct(vect3, vect1) != 0.f) || (Vect3f::scalarProduct(vect3, vect2) != 0.f))
+	{
+		throw 2;
+	}
 
-    vect3 = vect1 + vect2;
-    cout << "Add : vect3(" << vect3.x() << " " << vect3.y() << " " << vect3.z() << ")" << endl;
+	vect3 = vect1 + vect2;
+	cout << "Add : vect3(" << vect3.x() << " " << vect3.y() << " " << vect3.z() << ")" << endl;
 
-    vect3 = vect1 - vect2;
-    cout << "Subtract : vect3(" << vect3.x() << " " << vect3.y() << " " << vect3.z() << ")" << endl;
+	vect3 = vect1 - vect2;
+	cout << "Subtract : vect3(" << vect3.x() << " " << vect3.y() << " " << vect3.z() << ")" << endl;
 
-    vect3 = vect3 * 10.f;
-    cout << "Resize x10 : vect3(" << vect3.x() << " " << vect3.y() << " " << vect3.z() << ")" << endl;
+	vect3 = vect3 * 10.f;
+	cout << "Resize x10 : vect3(" << vect3.x() << " " << vect3.y() << " " << vect3.z() << ")" << endl;
 
-    vect3 = vect3 / 5.f;
-    cout << "Reducing /5 : vect3(" << vect3.x() << " " << vect3.y() << " " << vect3.z() << ")" << endl;
+	vect3 = vect3 / 5.f;
+	cout << "Reducing /5 : vect3(" << vect3.x() << " " << vect3.y() << " " << vect3.z() << ")" << endl;
 }
 using FieldConverter::Config;
 
 int main()
 {
-    /*
-    Vect3f vect1;
-    Vect3f vect2(10, 5, 5);
+	/*
+	Vect3f vect1;
+	Vect3f vect2(10, 5, 5);
+	run(vect1, vect2);
+	vect1.x(-15);
+	vect1.y(5);
+	vect1.z(-15);
+	run(vect1, vect2);
+	Vertex vertexSample;
+	*/
+	/*
+	HeightMap heightmapFile("Ressources/TestHeightMap.raw");
+	/* for (int i = 0; i < heightmapFile.size(); i++)
+	HeightMap heightmapFile("Ressources/TestHeightMap.raw");
+	map<unsigned int, Vertex> myVertexMap = heightmapFile.transformToVertexMap();
 
-
-    run(vect1, vect2);
-
-
-    vect1.x(-15);
-    vect1.y(5);
-    vect1.z(-15);
-
-    run(vect1, vect2);
-
-
-    Vertex vertexSample;
-    */
-    /*
-    HeightMap heightmapFile("Ressources/TestHeightMap.raw");
-
-   /* for (int i = 0; i < heightmapFile.size(); i++)
-    HeightMap heightmapFile("Ressources/TestHeightMap.raw");
-    map<unsigned int, Vertex> myVertexMap = heightmapFile.transformToVertexMap();
-    
-    for(int i = 0; i < heightmapFile.size(); i++)
-    {
-        cout << i << " : " << myVertexMap[i].toString();
-        cout << endl;
-    }
-
-    cout << endl;*/
-
-    /* convertir en VertexArray*/
-    VertexArray vArray = VertexArray(heightmapFile, 0.3f);
-    std::vector<Vertex> vList = vArray.getArray();
-   /* for (int i = 0;i < heightmapFile.size();i++)
-    {
-        //if (i % 3 == 2)
-        {
-            cout << vList[i].position().x();
-            cout << " ";
-            cout << vList[i].position().y();
-            cout << " ";
-            cout << vList[i].position().z();
-            cout << endl;
-        }
-    }*/
-
-    for (int i = 0; i < heightmapFile.size(); i++)
-    {
-        if (vList[i].position().z() < 0.f)
-        {
-            cout << vList[i].position().x();
-            cout << " ";
-            cout << vList[i].position().y();
-            cout << " ";
-            cout << vList[i].position().z();
-            cout << endl;
-        }
-    }
-
-
-    /*  LireFichierHeightmap();
-      ConstruireTerrain(float echelleXY, float echelleZ);
-      CalculerNormales();
-      ConstruireIndex();
-      EnregistrerTout();
-      */
-    Triangle triangle(10, 5, 2);
-
-    cout << triangle.toString();
-
-    /*TriangleArray mapArray (5, 5);
-
-    //cout << mapArray.numberOfPolygone() << " " << mapArray.numberOfPolygoneInColumn() << " " << mapArray.numberOfPolygoneInRow() << " " << mapArray.numberOfTilesInColumn() << " " << mapArray.numberOfTilesInRow() << endl;
-
-    cout << mapArray.toString();*/
-
-    UnitaryTest::TriangleArrayUnitTest::run();
-
-    cout << UnitaryTest::TriangleArrayUnitTest::getResult();
-
-  /*  LireFichierHeightmap();
-    ConstruireTerrain(float echelleXY, float echelleZ);
-    CalculerNormales();
-    ConstruireIndex();
-    EnregistrerTout();
-    */
-	try
+	for(int i = 0; i < heightmapFile.size(); i++)
 	{
-		throw std::runtime_error("lol");
+	cout << i << " : " << myVertexMap[i].toString();
+	cout << endl;
 	}
-	catch (const std::exception  &e)
+	cout << endl;*/
+
+	/* convertir en VertexArray*/
+	VertexArray vArray = VertexArray(heightmapFile, 0.3f);
+	std::vector<Vertex> vList = vArray.getArray();
+	/* for (int i = 0;i < heightmapFile.size();i++)
 	{
+	//if (i % 3 == 2)
+	{
+	cout << vList[i].position().x();
+	cout << " ";
+	cout << vList[i].position().y();
+	cout << " ";
+	cout << vList[i].position().z();
+	cout << endl;
 	}
+	}*/
+
+	for (int i = 0; i < heightmapFile.size(); i++)
+	{
+		if (vList[i].position().z() < 0.f)
+		{
+			cout << vList[i].position().x();
+			cout << " ";
+			cout << vList[i].position().y();
+			cout << " ";
+			cout << vList[i].position().z();
+			cout << endl;
+		}
+	}
+
+
+	/*  LireFichierHeightmap();
+	ConstruireTerrain(float echelleXY, float echelleZ);
+	CalculerNormales();
+	ConstruireIndex();
+	EnregistrerTout();
+	*/
+	Triangle triangle(10, 5, 2);
+
+	cout << triangle.toString();
+
+	/*TriangleArray mapArray (5, 5);
+	//cout << mapArray.numberOfPolygone() << " " << mapArray.numberOfPolygoneInColumn() << " " << mapArray.numberOfPolygoneInRow() << " " << mapArray.numberOfTilesInColumn() << " " << mapArray.numberOfTilesInRow() << endl;
+	cout << mapArray.toString();*/
+
+	UnitaryTest::TriangleArrayUnitTest::run();
+
+	cout << UnitaryTest::TriangleArrayUnitTest::getResult();
+
+	/*  LireFichierHeightmap();
+	ConstruireTerrain(float echelleXY, float echelleZ);
+	CalculerNormales();
+	ConstruireIndex();
+	EnregistrerTout();
+	*/
 
 	std::cout << Config::getInstance().getPath() << std::endl;
 	std::cout << Config::getInstance().getWidth() << std::endl;
 	std::cout << Config::getInstance().getHeight() << std::endl;
 	std::cout << Config::getInstance().getScale() << std::endl;
->>>>>>> Stashed changes
 
+	system("pause");
 
-    return 0;
+	return 0;
 }
